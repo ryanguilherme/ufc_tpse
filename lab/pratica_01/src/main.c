fdb10eba66315e47590795603e62c0541326a765
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-05-14 16:09:35 -0300

    otimizados diretorios

--- lab/pratica_01/src/main.c
+++ lab/pratica_01/src/main.c
@@ -1,7 +1,23 @@
-#include <stdio.h>
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  26-05-2015 17:31:15
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Francisco Helder (), helderhdw@gmail.com 
+ *   Organization:  Federal University of Ceara
+ *
+ * =====================================================================================
+ */
 
-int main(){
-	printf("Hello World");
-	return 0;
+int main (){
+
+
+	return(0);
 }
-
