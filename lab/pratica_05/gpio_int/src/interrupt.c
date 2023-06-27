ea2b0600a1ce585f38b9fa541c91699f4f3b7b37
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-06-27 13:02:07 -0300

    interrupt gpio practice

---
+++ lab/pratica_05/gpio_int/src/interrupt.c
@@ -0,0 +1,34 @@
+#include "interrupt.h"
+#include "bbb_regs.h"
+#include "hw_types.h"
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  gpioIsrHandler
+ *  Description:  
+ * =====================================================================================
+ */
+void gpioIsrHandler(void){
+
+    /* Clear the status of the interrupt flags */
+	HWREG(GPIO1_IRQSTATUS_0) = 0x10000000; 
+
+	flag_gpio = true;
+}
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  ISR_Handler
+ *  Description:  
+ * =====================================================================================
+ */
+void ISR_Handler(void){
+	/* Verify active IRQ number */
+	unsigned int irq_number = HWREG(INTC_SIR_IRQ) & 0x7f;
+	
+	if(irq_number == 98)
+		gpioIsrHandler();
+    
+	/* acknowledge IRQ */
+	HWREG(INTC_CONTROL) = 0x1;
+}
